d5701621fc0098664922d68e4616f7766502ee47
shahsuresh <shahsuresh75@gmail.com>
2024-09-05 15:31:47 +0545

    queryclient, redux provider code added

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,24 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import guestRoutes from "./routes/guestRoutes.jsx";
+import mainRoutes from "./routes/mainRoutes.jsx";
+import { Provider } from "react-redux";
+import reduxStore from "./store/store";
 
-createRoot(document.getElementById("root")).render(<App />);
+//? Create a client to use tanstack/react-query and Provide the client to your App
+const queryClient = new QueryClient();
+
+//? create router to use react-router-dom package for client side routing
+const router = createBrowserRouter([...guestRoutes, ...mainRoutes]);
+
+createRoot(document.getElementById("root")).render(
+  //   Provide the queryClient to your App from tanstack/react query
+  //   provider of redux store
+  <Provider store={reduxStore}>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </Provider>
+);
