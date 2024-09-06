f281cb8a6d2096dd0b2c83414b2eac8f83ec792b
shahsuresh <shahsuresh75@gmail.com>
2024-09-06 17:45:01 +0545

    Header and Footer component  designed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,97 @@
 import React from "react";
+import { Box, Stack, Typography, IconButton } from "@mui/material";
+import { Facebook, Twitter, Instagram, LinkedIn } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const navigate = useNavigate();
+  return (
+    <Box
+      component='footer'
+      sx={{
+        py: 1,
+        px: 1,
+        backgroundColor: "#1976D2",
+        borderTop: "1px solid #e7e7e7",
+        textAlign: "center",
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent='space-between'
+        alignItems='center'
+        spacing={2}
+        sx={{ maxWidth: "1200px", margin: "auto" }}
+      >
+        {/* Copyright */}
+        <Typography variant='h6' color='white' sx={{ fontFamily: "monospace" }}>
+          &copy; {new Date().getFullYear()} Namaste Home Tuition. All rights
+          reserved.
+        </Typography>
+
+        {/* Social Media Icons */}
+        <Stack direction='row' spacing={1}>
+          <IconButton
+            sx={{ color: "white" }}
+            component={Link}
+            href='https://www.facebook.com'
+            target='_blank'
+            rel='noopener'
+          >
+            <Facebook />
+          </IconButton>
+          <IconButton
+            sx={{ color: "white" }}
+            component={Link}
+            href='https://www.twitter.com'
+            target='_blank'
+            rel='noopener'
+          >
+            <Twitter />
+          </IconButton>
+          <IconButton
+            sx={{ color: "white" }}
+            component={Link}
+            href='https://www.instagram.com'
+            target='_blank'
+            rel='noopener'
+          >
+            <Instagram />
+          </IconButton>
+          <IconButton
+            sx={{ color: "white" }}
+            component={Link}
+            href='https://www.linkedin.com'
+            target='_blank'
+            rel='noopener'
+          >
+            <LinkedIn />
+          </IconButton>
+        </Stack>
+
+        {/* Contact Link */}
+
+        <Link to='/contact'>
+          <Typography
+            variant='h5'
+            sx={{
+              color: "white",
+              "&:hover": {
+                backgroundColor: "#116bc5",
+                textDecoration: "none",
+              },
+            }}
+          >
+            Contact Us
+          </Typography>
+        </Link>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,205 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { useNavigate } from "react-router-dom";
+
+const pages = [
+  { id: 1, name: "Home", path: "/home" },
+  { id: 2, name: "Login", path: "/login" },
+  { id: 3, name: "About", path: "/about" },
+];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Header = () => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    //!==========FullScreen Appbar=======================
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <img
+            src='logo1.png'
+            alt='logo'
+            style={{
+              width: "70px",
+              height: "auto",
+              display: { xs: "none", md: "flex" },
+              mr: 1,
+            }}
+          />
+          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+          <Typography
+            variant='h4'
+            noWrap
+            component='a'
+            href='#app-bar-with-responsive-menu'
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Namaste Home Tuition
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.id}
+                  onClick={() => {
+                    navigate(page.path);
+                  }}
+                >
+                  <Typography sx={{ textAlign: "center" }}>
+                    {page.name}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* //!==========Drawer Screen======================= */}
+          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
+          {/* <img
+            src='logo1.png'
+            alt='logo'
+            style={{
+              width: "70px",
+              height: "auto",
+              display: { xs: "none", md: "flex", lg: "none" },
+              mr: 1,
+            }}
+          ></img> */}
+          <Typography
+            variant='h6'
+            noWrap
+            component='a'
+            href='#app-bar-with-responsive-menu'
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              fontSize: { xs: "11px", md: "18px" },
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Namaste Home Tuition
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                size='large'
+                key={page.id}
+                onClick={() => {
+                  navigate(page.path);
+                }}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  "&:hover": { backgroundColor: "#116bc5" },
+                }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title='Open settings'>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt='User' src='user.png' />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id='menu-appbar'
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: "center" }}>
+                    {setting}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
