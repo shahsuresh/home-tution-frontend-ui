de94c9849b1a897a71a8ce1dde23540c3ade30e4
shahsuresh <shahsuresh75@gmail.com>
2024-09-10 00:20:43 +0545

    get all tuitions data in teacher-profile page

---
+++ src/pages/TeacherProfile.jsx
@@ -0,0 +1,39 @@
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
+import React from "react";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import { useNavigate } from "react-router-dom";
+import TutionPostCard from "../components/TutionPostCard";
+
+const TeacherProfile = () => {
+  const userRole = localStorage.getItem("role");
+  const navigate = useNavigate();
+
+  return (
+    <Box className='flex flex-col items-center justify-center w-full gap-1'>
+      {userRole === "teacher" && (
+        <Box className='flex items-center justify-center mt-1'>
+          <Button
+            variant='contained'
+            color='primary'
+            size='medium'
+            endIcon={<ImportContactsIcon />}
+            onClick={() => {
+              navigate("/add-tuition");
+            }}
+          >
+            Add Tuition
+          </Button>
+        </Box>
+      )}
+
+      <Box className='flex flex-col items-center justify-center w-10/12 gap-1 bg-[#1976D2] '>
+        <Typography variant='h5' className='text-white '>
+          Lists of Tuitions, you have added
+        </Typography>
+        <TutionPostCard />
+      </Box>
+    </Box>
+  );
+};
+
+export default TeacherProfile;
