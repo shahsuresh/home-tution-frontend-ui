2879853234a9b6dc076e6e490053ad8c0b3b0357
shahsuresh <shahsuresh75@gmail.com>
2024-09-10 00:19:19 +0545

    register page added for teachers

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,6 +4,7 @@ import {
   FormControl,
   FormHelperText,
   InputLabel,
+  LinearProgress,
   MenuItem,
   Select,
   TextField,
@@ -11,10 +12,35 @@ import {
 } from "@mui/material";
 import { Formik } from "formik";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { registerTeacherValidationSchema } from "../validationSchema/teacher.login.validation.schema";
+import { useMutation } from "@tanstack/react-query";
+import $axios from "../lib/axios/axios.instance";
+import { useDispatch } from "react-redux";
+import {
+  openErrorSnackbar,
+  openSuccessSnackbar,
+} from "../store/slices/snackbarSlice";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  //?=== hit register api =======
+  const { isPending, mutate } = useMutation({
+    mutationKey: ["register-teacher"],
+    mutationFn: async (values) => {
+      return await $axios.post("/teacher/register", values);
+    },
+    onSuccess: (res) => {
+      dispatch(openSuccessSnackbar(res?.data?.message));
+      console.log(res.data.message);
+      navigate("/login");
+    },
+    onError: (error) => {
+      dispatch(openErrorSnackbar(error?.message));
+      console.log("Error", error);
+    },
+  });
   return (
     <>
       <Box className='flex flex-col items-center justify-center'>
@@ -33,7 +59,11 @@ const Register = () => {
           }}
           validationSchema={registerTeacherValidationSchema}
           onSubmit={(values) => {
-            console.log(values);
+            const subjectsArray = values.subjects
+              .split(",")
+              .map((subject) => subject.trim());
+            values.subjects = subjectsArray;
+            mutate(values);
           }}
         >
           {(formik) => (
@@ -41,6 +71,8 @@ const Register = () => {
               className='flex flex-col gap-2 p-3 mt-10 w-1/3 min-w-fit h-auto rounded-lg bg-gradient-to-r from-blue-40 shadow-xl shadow-blue-500/50 mb-10'
               onSubmit={formik.handleSubmit}
             >
+              {isPending && <LinearProgress color='primary' />}
+
               <Typography variant='h4' className='text-[#1976D2]'>
                 Sign up
               </Typography>
@@ -146,7 +178,7 @@ const Register = () => {
 
               <FormControl>
                 <TextField
-                  label='Subjects'
+                  label='Subjects(comma-separated)'
                   required
                   {...formik.getFieldProps("subjects")}
                 />
@@ -185,7 +217,7 @@ const Register = () => {
                 variant='contained'
                 color='secondary'
                 type='submit'
-                disabled={false}
+                disabled={isPending}
               >
                 Register
               </Button>
