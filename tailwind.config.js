37dbbf281fe8bdcb9d9a41d38f49233e4e4da127
shahsuresh <shahsuresh75@gmail.com>
2024-09-11 16:31:16 +0545

    google fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         xxl: "1600px", // Add a custom screen size 'xxl' with a minimum width of 1600px
         xs: "360px", // Add another custom screen size 'xs' with a minimum width of 475px
       },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        Playpen_Sans: ["Playpen Sans"],
+      },
     },
   },
   plugins: [],
