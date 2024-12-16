143ed3e10842603d3b9e86c8a8153e75efd6cfe1
shahsuresh <shahsuresh75@gmail.com>
2024-12-16 15:48:11 +0545

    access backend-uri from .env file

--- src/lib/axios/axios.instance.js
+++ src/lib/axios/axios.instance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // axios instance
 const $axios = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
+  // "http://localhost:5000",
   timeout: 5000,
 });
 
